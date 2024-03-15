12637ff629a4a3059dc06ada3a348b46a3554db0
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-03-16 10:40:22 +1100

    loading component added

--- src/app/snippet/[id]/page.tsx
+++ src/app/snippet/[id]/page.tsx
@@ -10,10 +10,10 @@ interface SnippetShowPageProps {
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps) {
+  await new Promise((r) => setTimeout(r, 2000));
   const snippet = await db.snippet.findFirst({
     where: { id: +props.params.id },
   });
-  console.log(snippet);
 
   if (!snippet) {
     return notFound();
