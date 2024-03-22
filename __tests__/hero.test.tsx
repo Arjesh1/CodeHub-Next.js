c64c643891ffd14618d0ff088e2b17c446fc33c3
Arjesh Khadka <arjesh.khadka2@gmail.com>
2024-03-22 21:00:29 +1100

    test for hero component added

---
+++ __tests__/hero.test.tsx
@@ -0,0 +1,45 @@
+import {
+  screen,
+  render,
+  fireEvent,
+  getAllByRole,
+} from "@testing-library/react";
+import { Hero } from "../src/components/hero";
+
+describe("Hero component", () => {
+  it("renders the component", () => {
+    render(<Hero />);
+    const linkElement = screen.getAllByRole("link");
+    const buttonElement = screen.getAllByRole("button");
+
+    expect(linkElement).toHaveLength(3);
+    expect(buttonElement).toHaveLength(3);
+  });
+
+  it("checks for add button and link", () => {
+    const { getByText, getByRole } = render(<Hero />);
+    const linkElement = getByRole("link", { name: "+ Add" });
+    const buttonText = getByText("+ Add");
+
+    expect(buttonText).toBeInTheDocument();
+    expect(linkElement.getAttribute("href")).toBe("snippet/add");
+  });
+
+  it("checks for me button and link", () => {
+    const { getByText, getByRole } = render(<Hero />);
+    const linkElement = getByRole("link", { name: "Me" });
+    const buttonText = getByText("Me");
+
+    expect(buttonText).toBeInTheDocument();
+    expect(linkElement.getAttribute("href")).toBe("/");
+  });
+
+  it("checks for explore button and link", () => {
+    const { getByText, getByRole } = render(<Hero />);
+    const linkElement = getByRole("link", { name: "Explore" });
+    const buttonText = getByText("Explore");
+
+    expect(buttonText).toBeInTheDocument();
+    expect(linkElement.getAttribute("href")).toBe("/explore");
+  });
+});
